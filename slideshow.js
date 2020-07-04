ba916aea06e232136531a81aa16fdff757379e78
nicc00 <65678327+nicc00@users.noreply.github.com>
2020-07-04 14:43:50 -0700

    changed la haine more pictures to slideshow

---
+++ slideshow.js
@@ -0,0 +1,35 @@
+const currentSlide = document.getElementById("currentSlide");
+const slides = [
+  "images/lahaine3.jpeg",
+  "images/lahaine4.jpg",
+  "images/lahaine5.jpg",
+  "images/lahaine6.jpg",
+];
+let slide = 0;
+let next = document.getElementById("next");
+let previous = document.getElementById("previous");
+let slideshow = document.getElementById("slideshow");
+function doSlideshow(n) {
+  slide += n;
+  if (slide > slides.length - 1) {
+    slide = 0;
+  }
+  if (slide < 0) {
+    slide = slides.length - 1;
+  }
+  currentSlide.src = slides[slide];
+}
+next.onclick = function () {
+  doSlideshow(1);
+};
+previous.onclick = function () {
+  doSlideshow(-1);
+};
+function morePictures() {
+  if (slideshow.style.display === "block") {
+    slideshow.style.display = "none";
+  } else {
+    slideshow.style.display = "block";
+  }
+}
+document.getElementById("morePictures").onclick = morePictures;
