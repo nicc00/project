f9ff40227ffe57a51cca86854cdf690dfc981c2f
nicc00 <65678327+nicc00@users.noreply.github.com>
2020-07-05 15:04:27 -0700

    added final requirements

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,7 @@ let slide = 0;
 let next = document.getElementById("next");
 let previous = document.getElementById("previous");
 let slideshow = document.getElementById("slideshow");
-function doSlideshow(n) {
+function doSlideshow(n, a) {
   slide += n;
   if (slide > slides.length - 1) {
     slide = 0;
@@ -12,18 +12,33 @@ function doSlideshow(n) {
     slide = slides.length - 1;
   }
   currentSlide.src = slides[slide];
+  if (a === 1) {
+      timer = setTimeout(function(){doSlideshow(1, 1);}, 3000)
+  }
+  else if (a === 2){
+      clearTimeout(timer)
+  }
 }
 next.onclick = function () {
-  doSlideshow(1);
+  doSlideshow(1, 0);
 };
 previous.onclick = function () {
-  doSlideshow(-1);
+  doSlideshow(-1, 0);
+};
+currentSlide.onmouseover = function () {
+    doSlideshow(1, 1);
+};
+currentSlide.onmouseout = function () {
+    doSlideshow(0, 2);
 };
 function morePictures() {
   if (slideshow.style.display === "block") {
     slideshow.style.display = "none";
+    document.getElementById("morePictures").innerHTML = "more pictures"
+
   } else {
     slideshow.style.display = "block";
+    document.getElementById("morePictures").innerHTML = "hide pictures"
   }
 }
 document.getElementById("morePictures").onclick = morePictures;
